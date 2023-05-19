87fa4bedaa6c7a9b5794d54549a665fbcc488f58
Anas-Ouardi <anasouardi101@gmail.com>
2023-05-19 21:29:30 +0100

    Multi Auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,52 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function login(Request $request){
+
+        $input = $request->all();
+        $this->validate($request,[
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $input['email'])->first();
+        if (!$user) {
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ])->onlyInput('email');
+        }
+    
+        if ($user->status != 'active') {
+            return back()->withErrors([
+                'email' => 'Your account is BLOCKED. Please contact support for assistance.',
+            ])->onlyInput('email');
+        }
+
+        
+        if(auth()->attempt(['email'=>$input["email"],'password'=>$input["password"]],$request->remember))
+        {
+            
+            if(auth()->user()->role == 'admin')
+            {
+                return redirect()->route('Admin.dashboard');
+            }
+            else if(auth()->user()->role == 'client')
+            {
+                return redirect()->route('Client.dashboard');
+            }
+            
+            else{
+                return back()->withErrors([
+                    'email' => 'The provided credentials do not match our records.',
+                ])->onlyInput('email');
+            }
+        }
+        else
+        {
+           return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ])->onlyInput('email');
+        }
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string("status")->default("active");
+            $table->string("role")->default("client");
             $table->rememberToken();
             $table->timestamps();
         });
