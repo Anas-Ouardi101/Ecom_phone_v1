a259c8f2bc3cadbd5598b2d33effc18401278786
Anas-Ouardi <anasouardi101@gmail.com>
2023-05-20 13:03:55 +0100

    Admin dashbord and migration modifaided

--- app/Http/Controllers/Admin/AdminDashbord.php
+++ app/Http/Controllers/Admin/AdminDashbord.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class AdminDashbord extends Controller
 {
     public function index(){
-        return view("Admin.dashbord");
+        return view("Admin.main");
     }
 }
